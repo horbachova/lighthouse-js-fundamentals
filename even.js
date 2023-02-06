8e2b329ed1b21b490548fd48d03f33b087aaa98a
Yevheniia Horbachova <gor.jenia2003@gmail.com>
2023-02-05 19:00:46 -0700

    Function fo chech whether number is even or not

--- even.js
+++ even.js
@@ -1,6 +1,6 @@
-const isEven = function (num) {
+const isEven  = function (num) {
     return num % 2 === 0;
   }
   
-  console.log(isEven(100));
-  console.log(isEven(111));
+  console.log(isEven(10));
+  console.log(isEven(11));
